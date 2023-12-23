350f4a7e8c711c06422b548c4b2c448735445575
Chaher <chaher.croissance.public@gmail.com>
2023-12-23 20:54:04 +0100

    Initial Commit

---
+++ TimeShift/TimeShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeShiftApp.swift
+//  TimeShift
+//
+//  Created by Chaher Machhour on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TimeShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
